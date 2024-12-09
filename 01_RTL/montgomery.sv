d3c42181e0e3bca43086e9a1064a2bcc6e08f840
jialin <jlfang0119@gmail.com>
2024-12-10 00:21:38 +0800

    modify montgomery

--- 01_RTL/montgomery.sv
+++ 01_RTL/montgomery.sv
@@ -13,40 +13,40 @@ module Montgomery (
 
     state_t state_r, state_w;
     logic [256:0] tmp [0:1];
-    logic [256:0] m_r;
-    logic [256:0] m_w;
-    logic [254:0] a, b;
-    logic [254:0] o_montgomery_r;
-    logic o_finished_r;
+    logic [256:0] m_r, m_w;
+    logic [254:0] a_r, a_w, b_r, b_w;
+    logic o_finished_r, o_finished_w;
     logic [8:0] cycle_r, cycle_w;
     integer i;
 
+    wire [256:0] m_minus_N = m_r - `N;
+    wire [256:0] tmp0 = a_r[cycle_r] ? m_r + b_r : m_r;
+    wire [256:0] tmp1 = tmp0[0] ? tmp0 + `N : tmp0;
+
     assign o_finished = o_finished_r;
-    assign o_montgomery = o_montgomery_r;
+    assign o_montgomery = m_minus_N[256] ? m_r : m_minus_N;
 
     always_comb begin
         state_w = state_r;
         cycle_w = 0;
-        for (i = 0; i < 1; i = i + 1) begin
-            tmp[i] = 0;
-        end
+        a_w = a_r;
+        b_w = b_r;
         m_w = 0;
+        o_finished_w = 0;
         case(state_r)
             S_IDLE: begin
+                a_w = i_a;
+                b_w = i_b;
                 if(i_start) begin
                     state_w = S_CALC;
                 end
             end
             S_CALC: begin
                 cycle_w = cycle_r + 1;
-                if(cycle_r < 255) begin
-                    state_w = S_CALC;
-                    tmp[0] = a[cycle_r] ? m_r + b : m_r;
-                    tmp[1] = tmp[0][0] ? tmp[0] + `N : tmp[0];
-                    m_w = tmp[1] >> 1;
-                end else begin
+                m_w = tmp1 >> 1;
+                if(cycle_r == 254) begin
                     state_w = S_IDLE;
-                    cycle_w = 0;
+                    o_finished_w = 1;
                 end
             end
         endcase
@@ -54,27 +54,19 @@ module Montgomery (
 
     always_ff @(posedge i_clk) begin
         if (i_rst) begin
-            m_r <= 0;
             state_r <= S_IDLE;
             cycle_r <= 0;
+            a_r <= 0;
+            b_r <= 0;
+            m_r <= 0;
             o_finished_r <= 0;
-            o_montgomery_r <= 0;
-            a <= 0;
-            b <= 0;
         end else begin
-            o_finished_r <= 0;
-            o_montgomery_r <= (m_r >= {2'b0, `N}) ? m_r - `N : m_r;
             state_r <= state_w;
             cycle_r <= cycle_w;
-            if (state_r == S_IDLE && state_w == S_CALC) begin
-                a <= i_a;
-                b <= i_b;
-                m_r <= 0;
-            end else if (state_r == S_CALC && state_w == S_CALC) begin
-                m_r <= m_w;
-            end else if (state_r == S_CALC && state_w == S_IDLE) begin
-                o_finished_r <= 1;
-            end
+            a_r <= a_w;
+            b_r <= b_w;
+            m_r <= m_w;
+            o_finished_r <= o_finished_w;
         end
     end
 
